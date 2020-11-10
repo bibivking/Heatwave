ae08682293a9caa671e7cd63d50830c1f4a1fa5e
Mengyuan Mu <mu.mengyuan815@gmail.com>
2020-11-10 14:20:37 +1100

    backup

---
+++ HW_GW/hw-2017-2019.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#PBS -m ae
+#PBS -M mengyuan.mu@unsw.edu.au
+#PBS -P w35
+#PBS -q normal
+#PBS -l walltime=10:00:00
+#PBS -l mem=20GB
+#PBS -l ncpus=1
+#PBS -j oe
+#PBS -l wd
+#PBS -l storage=gdata/w35
+
+module load ncl/6.6.2
+
+cd /g/data/w35/mm3972/scripts/Heatwave/HW_GW
+
+ncl spitial_map_HESS_ver_comp_HW-4.ncl > hw-2017-2019
