5bbc332dfe5cf9542a4b1b332ef9553748e8ad12
Mengyuan Mu <mu.mengyuan815@gmail.com>
2020-12-08 13:20:08 +1100

    backup

--- HW_GW/submit_task.sh
+++ HW_GW/submit_task.sh
@@ -4,8 +4,8 @@
 #PBS -M mengyuan.mu@unsw.edu.au
 #PBS -P w35
 #PBS -q normal
-#PBS -l walltime=03:00:00
-#PBS -l mem=20GB
+#PBS -l walltime=05:00:00
+#PBS -l mem=40GB
 #PBS -l ncpus=1
 #PBS -j oe
 #PBS -l wd
@@ -15,4 +15,4 @@ module load ncl/6.6.2
 
 cd /g/data/w35/mm3972/scripts/Heatwave/HW_GW
 
-ncl spitial_map_HESS_version_compare_monthly.ncl
+ncl spitial_map_HESS_ver_comp_HW.ncl
