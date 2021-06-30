098792b61dd080fe56c16ae89b44aeef189ed581
Mengyuan Mu <mu.mengyuan815@gmail.com>
2021-06-30 14:27:10 +1000

    backup

--- GW_DH/plot_boxplots.py
+++ GW_DH/plot_boxplots.py
@@ -18,7 +18,7 @@ import numpy as np
 import seaborn as sns
 sns.set_theme(style="ticks", palette="pastel")
 
-def clean_files(fname, start_yr):
+def read_summer_heatwave(fname, start_yr):
 
     df = pd.read_csv(fname, header=None)
     df.columns = ["day", "var"]
@@ -27,18 +27,25 @@ def clean_files(fname, start_yr):
     start = date(start_yr,1,1)
     dates = []
     for i in range(len(df)):
-        # need the -1 or the first day is 17 not 16
-        delta = timedelta(int(df.day[i]) - 1)
+
+        delta = timedelta(int(df.day[i]) - 1) # days from start_yr-01-01
         offset = start + delta
         dates.append(offset)
 
     df["dates"] = dates
     #df = df.set_index('dates')
+
     df.index = pd.to_datetime(df.dates, format = '%Y/%m/%d', utc=False)
     df["year"] = df.index.year
-
-    ofname = fname.replace("txt", "csv")
-    df.to_csv(ofname, index=True)
+    df_summer_hw = df[np.any([ np.all( [ df.index.month == 12, df.index.year < 2019 ], axis=0), 
+                               np.all( [ df.index.month <= 2,  df.index.year > 2000 ], axis=0) ], axis=0)]
+    
+    for year in np.arange(2000,2019):
+        # df_summer_hw[np.all([df_summer_hw.index.month == 12, df_summer_hw.index.year == year], axis=0)]['year'] = year + 1
+        df_summer_hw['year'].loc[np.all([df_summer_hw.index.month == 12, df_summer_hw.index.year == year], axis=0)] = year + 1
+       
+    print(df_summer_hw)
+    return df_summer_hw
 
 def align_yaxis(ax1, v1, ax2, v2):
     """adjust ax2 ylimit so that v2 in ax2 is aligned to v1 in ax1"""
@@ -49,75 +56,151 @@ def align_yaxis(ax1, v1, ax2, v2):
     miny, maxy = ax2.get_ylim()
     ax2.set_ylim(miny+dy, maxy+dy)
 
-def Fig3_boxplot():
-    fname = "EF_FD_rawdata_4_Python.csv"
-    df_fd = pd.read_csv(fname)
-    df_fd['experiment'] = "FD"
-    fname = "EF_GW_rawdata_4_Python.csv"
-    df_gw = pd.read_csv(fname)
-    df_gw['experiment'] = "GW"
+def Fig3_boxplot(start_yr,var_names,ylabels,ylabels_R,ranges,ranges_diff):
 
-    # you can omit, or do something else with this bit later
-    df_fd.temp = np.where(df_fd.temp > 3.8, df_fd.temp == 3.8, df_fd.temp)
-    df_fd.temp = np.where(df_fd.temp < 0.0, df_fd.temp == 0.0, df_fd.temp)
-    df_gw.temp = np.where(df_gw.temp > 3.8, df_gw.temp == 3.8, df_gw.temp)
-    df_gw.temp = np.where(df_gw.temp < 0.0, df_gw.temp == 0.0, df_gw.temp)
-
-    medians_gw = df_gw.groupby(['year'])['temp'].median().values
-    medians_fd = df_fd.groupby(['year'])['temp'].median().values
-
-
-    # make one dataframe
-    df = pd.concat([df_fd, df_gw])
-    df.drop(['dates.1'], axis=1)
-
+    # set plots
     sns.set_style("ticks")
     sns.set_style({"xtick.direction": "in","ytick.direction": "in"})
 
-    fig = plt.figure(figsize=(12,6))
-    fig.subplots_adjust(hspace=0.1)
-    fig.subplots_adjust(wspace=0.05)
-    plt.rcParams['text.usetex'] = False
-    plt.rcParams['font.family'] = "sans-serif"
-    plt.rcParams['font.sans-serif'] = "Helvetica"
-    plt.rcParams['axes.labelsize'] = 14
-    plt.rcParams['font.size'] = 12
+    fig, axs = plt.subplots(2, 2, figsize=(10, 7))
+
+
+    # fig = plt.figure(figsize=(12,6))
+    plt.rcParams['text.usetex']     = False
+    plt.rcParams['font.family']     = "sans-serif"
+    plt.rcParams['font.serif']      = "Helvetica"
+    plt.rcParams['axes.linewidth']  = 1.5
+    plt.rcParams['axes.labelsize']  = 14
+    plt.rcParams['font.size']       = 14
     plt.rcParams['legend.fontsize'] = 12
-    plt.rcParams['xtick.labelsize'] = 12
-    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+    plt.rcParams["legend.markerscale"] = 3.0
 
-    ax = fig.add_subplot(111)
-    ax2 = ax.twinx()
+    almost_black = '#262626'
+    # change the tick colors also to the almost black
+    plt.rcParams['ytick.color'] = almost_black
+    plt.rcParams['xtick.color'] = almost_black
 
-    ax = sns.boxplot(x="year", y="temp", data=df,
-                     showfliers=False, palette=["m", "g"],
-                     hue="experiment", whis=0)
-    #ax.fill_between(np.arange(2000, 2010), 0, 1, facecolor="lightgrey", alpha=0.5)
-    ax2.plot(medians_gw-medians_fd, ls="-", color="black", label="GW-FD")
-    #align_yaxis(ax, 0, ax2, 0)
+    # change the text colors also to the almost black
+    plt.rcParams['text.color']  = almost_black
 
-    ax.set_ylim(0, 1.0)
-    #ax2.set_ylim(0, 0.2)
+    # Change the default axis colors from black to a slightly lighter black,
+    # and a little thinner (0.5 instead of 1)
+    plt.rcParams['axes.edgecolor']  = almost_black
+    plt.rcParams['axes.labelcolor'] = almost_black
 
-    xxlim = ax.get_xlim()
-    #print(xxlim)
-    ax.fill_between([-0.5, 9], 0, 0.8, facecolor="lightgrey", alpha=0.3)
-    ax.fill_between([16.5, 19.5], 0, 0.8, facecolor="lightgrey", alpha=0.3)
-    #ax.axhspan(0, 1, facecolor="lightgrey", alpha=0.5)
+    # set the box type of sequence number
+    props = dict(boxstyle="round", facecolor='white', alpha=0.0, ec='white')
+    #colors = cm.Set2(np.arange(0,len(case_labels)))
 
-    #ax.axvspan(2000, 2009, 0, 1, facecolor="lightgrey", alpha=0.5)
+    # ax = fig.add_subplot(111)
+    # ax2 = ax.twinx()
 
-    ax.set_ylabel('EF ($\degree$C)')
-    ax2.set_ylabel('Temperature ($\degree$C)')
-    ax.set_xlabel(" ")
-    #ax.legend(numpoints=1, loc="best", frameon=False)
-    ax.legend(numpoints=1, loc=(0.7, 0.8), frameon=False)
+    orders  = ['(a)','(b)','(c)','(d)']
 
-    for ind, label in enumerate(ax.get_xticklabels()):
-        if ind % 2 == 0:  # every 2nd label
-            label.set_visible(True)
+    for i, var_name in enumerate(var_names):
+
+        row = i//2 # round
+        col = i%2  # mod
+
+        filename_GW = "./txt/"+var_name+"_GW_rawdata_4_Python.txt"
+        filename_FD = "./txt/"+var_name+"_FD_rawdata_4_Python.txt"
+
+        df_gw = read_summer_heatwave(filename_GW, start_yr)
+        df_gw['experiment'] = "GW"
+        df_fd = read_summer_heatwave(filename_FD, start_yr)
+        df_fd['experiment'] = "FD"
+
+        axs2 = axs[row,col].twinx()
+        # # you can omit, or do something else with this bit later
+        # df_fd.var = np.where(df_fd.var > 3.8, df_fd.var == 3.8, df_fd.var)
+        # df_fd.var = np.where(df_fd.var < 0.0, df_fd.var == 0.0, df_fd.var)
+        # df_gw.var = np.where(df_gw.var > 3.8, df_gw.var == 3.8, df_gw.var)
+        # df_gw.var = np.where(df_gw.var < 0.0, df_gw.var == 0.0, df_gw.var)
+
+        medians_gw = df_gw.groupby(['year'])['var'].median().values
+        medians_fd = df_fd.groupby(['year'])['var'].median().values
+
+
+        # make one dataframe
+        df = pd.concat([df_gw,df_fd])
+        print(df)
+        # df.drop(['dates.1'], axis=1)
+
+        axs[row,col] = sns.boxplot(x="year", y="var", data=df,
+                        showfliers=False, palette=["m", "g"],
+                        hue="experiment", whis=0)
+        #ax.fill_between(np.arange(2000, 2010), 0, 1, facecolor="lightgrey", alpha=0.5)
+        axs2.plot(medians_gw-medians_fd, ls="-", color=almost_black, label="GW-FD")
+        #align_yaxis(ax, 0, ax2, 0)
+
+        print(ranges[i])
+        axs[row,col].set_ylim(ranges[i])
+        axs2.set_ylim(ranges_diff[i])
+
+        xxlim = axs[row,col].get_xlim()
+        #print(xxlim)
+        fill_color = (1., 0.972549, 0.862745) # named color "cornsilk" in ncl
+        axs[row,col].fill_between([ 0.5, 8.5], ranges[i][0], ranges[i][1], facecolor=fill_color, alpha=0.5)
+        axs[row,col].fill_between([16.5, 19.5], ranges[i][0], ranges[i][1], facecolor=fill_color, alpha=0.5)
+        #ax.axhspan(0, 1, facecolor="lightgrey", alpha=0.5)
+
+        #ax.axvspan(2000, 2009, 0, 1, facecolor="lightgrey", alpha=0.5)
+
+        # ytickslocs     = [  0,    50,  100,   150,  200,   250,  300,   350,  400,  450]
+        # yticklabels    = ["0.0", "0.5",  "1.0", "1.5",  "2.0", "2.5",  "3.0", "3.5",  "4.0", "4.5"]
+
+
+        # axs[row,col].set(yticks=ytickslocs, yticklabels=yticklabels)
+
+        axs[row,col].set_ylabel(ylabels[i])
+        axs[row,col].set_xlabel(" ")
+        # axs[row,col].legend(numpoints=1, loc="best", frameon=False)
+
+        axs2.set_ylabel(ylabels_R[i])
+
+
+        xtickslocs     = [    0,   1,      2,  3,      4,  5,      6,  7,      8,  9, 
+                             10,  11,     12,  13,    14, 15,     16, 17,     18     ]
+        xticklabels    = ["2001", "", "2003", "", "2005", "", "2007", "", "2009", "",
+                          "2011", "", "2013", "", "2015", "", "2017", "", "2019"     ]
+        # plt.setp(axs[row,col].get_xticklabels(), visible=False)
+        axs[row,col].get_xaxis().set_visible(False)
+        if row == 0 and col ==0:
+            axs[row,col].legend(numpoints=1, loc="best", frameon=False) # loc=(0.7, 0.8) 
         else:
-            label.set_visible(False)
+            axs[row,col].get_legend().remove()
 
-    of = "/Users/mdekauwe/Desktop/blah.png"
-    fig.savefig(of, bbox_inches='tight', dpi=300, pad_inches=0.1)
+        if row == 1:    
+            # plt.setp(axs[row,col].get_xticklabels(), visible=False)
+            axs[row,col].get_xaxis().set_visible(True)
+            axs[row,col].set(xticks=xtickslocs, xticklabels=xticklabels)
+
+        axs[row,col].text(0.05, 0.95, orders[i],transform=axs[row,col].transAxes, fontsize=12, verticalalignment='top', bbox=props) # 
+
+       
+        # for ind, label in enumerate(axs[row,col].get_xticklabels()):
+        #     if ind % 2 == 0:  # every 2nd label
+        #         label.set_visible(True)
+        #         plt.setp(axs[row,col].get_xticklabels(), visible=True)
+        #         axs[row,col].set(xticks=xtickslocs, xticklabels=xticklabels)
+        #     else:
+        #         label.set_visible(False)
+        #         plt.setp(axs[row,col].get_xticklabels(), visible=True)
+        #         axs[row,col].set(xticks=xtickslocs, xticklabels=xticklabels)
+
+    fig.savefig("./plots/plot_boxplots.png", bbox_inches='tight', dpi=300, pad_inches=2) # 
+
+
+if __name__ == "__main__":
+
+
+    var_names   = ["deltaT","EF","TVeg","Fwsoil"]
+    ylabels     = ["ΔT (oC)","EF (-)","Et (mm d-1)","β (-)"]
+    ylabels_R   = ["ΔT_GW - ΔT_FD (oC)","ΔEF (-)","ΔEt (mm d-1)","Δβ (-)"]
+    ranges      = [[-0.5, 5], [0., 0.8], [0., 3.8], [0., 1.05]]
+    ranges_diff = [[-0.8, 0.], [0., 0.2], [0., 1.1], [0., 0.4]]
+    start_yr    = 2000
+
+    Fig3_boxplot(start_yr,var_names,ylabels,ylabels_R,ranges,ranges_diff)
