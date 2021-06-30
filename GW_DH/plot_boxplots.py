1e365874b18566941e586a29d6fbd577fc19077f
Mengyuan Mu <mu.mengyuan815@gmail.com>
2021-06-30 16:14:58 +1000

    need to compare with previous version

--- GW_DH/plot_boxplots.py
+++ GW_DH/plot_boxplots.py
@@ -16,7 +16,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-sns.set_theme(style="ticks", palette="pastel")
+
 
 def read_summer_heatwave(fname, start_yr):
 
@@ -61,20 +61,20 @@ def Fig3_boxplot(start_yr,var_names,ylabels,ylabels_R,ranges,ranges_diff):
     # set plots
     sns.set_style("ticks")
     sns.set_style({"xtick.direction": "in","ytick.direction": "in"})
+    sns.set_theme(style="ticks", palette="pastel")
 
     fig, axs = plt.subplots(2, 2, figsize=(10, 7))
-
 
     # fig = plt.figure(figsize=(12,6))
     plt.rcParams['text.usetex']     = False
     plt.rcParams['font.family']     = "sans-serif"
     plt.rcParams['font.serif']      = "Helvetica"
     plt.rcParams['axes.linewidth']  = 1.5
-    plt.rcParams['axes.labelsize']  = 14
-    plt.rcParams['font.size']       = 14
+    plt.rcParams['axes.labelsize']  = 12
+    plt.rcParams['font.size']       = 12
     plt.rcParams['legend.fontsize'] = 12
-    plt.rcParams['xtick.labelsize'] = 14
-    plt.rcParams['ytick.labelsize'] = 14
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
     plt.rcParams["legend.markerscale"] = 3.0
 
     almost_black = '#262626'
@@ -104,6 +104,9 @@ def Fig3_boxplot(start_yr,var_names,ylabels,ylabels_R,ranges,ranges_diff):
         row = i//2 # round
         col = i%2  # mod
 
+        # -------------------- boxplot ---------------------
+
+        # read box values
         filename_GW = "./txt/"+var_name+"_GW_rawdata_4_Python.txt"
         filename_FD = "./txt/"+var_name+"_FD_rawdata_4_Python.txt"
 
@@ -112,61 +115,30 @@ def Fig3_boxplot(start_yr,var_names,ylabels,ylabels_R,ranges,ranges_diff):
         df_fd = read_summer_heatwave(filename_FD, start_yr)
         df_fd['experiment'] = "FD"
 
-        axs2 = axs[row,col].twinx()
-        # # you can omit, or do something else with this bit later
-        # df_fd.var = np.where(df_fd.var > 3.8, df_fd.var == 3.8, df_fd.var)
-        # df_fd.var = np.where(df_fd.var < 0.0, df_fd.var == 0.0, df_fd.var)
-        # df_gw.var = np.where(df_gw.var > 3.8, df_gw.var == 3.8, df_gw.var)
-        # df_gw.var = np.where(df_gw.var < 0.0, df_gw.var == 0.0, df_gw.var)
-
-        medians_gw = df_gw.groupby(['year'])['var'].median().values
-        medians_fd = df_fd.groupby(['year'])['var'].median().values
-
-
         # make one dataframe
         df = pd.concat([df_gw,df_fd])
         print(df)
-        # df.drop(['dates.1'], axis=1)
 
+        # Plotting boxplot
         axs[row,col] = sns.boxplot(x="year", y="var", data=df,
                         showfliers=False, palette=["m", "g"],
                         hue="experiment", whis=0)
-        #ax.fill_between(np.arange(2000, 2010), 0, 1, facecolor="lightgrey", alpha=0.5)
-        axs2.plot(medians_gw-medians_fd, ls="-", color=almost_black, label="GW-FD")
-        #align_yaxis(ax, 0, ax2, 0)
-
-        print(ranges[i])
+        # xxlim = axs[row,col].get_xlim()
         axs[row,col].set_ylim(ranges[i])
-        axs2.set_ylim(ranges_diff[i])
+        axs[row,col].set_ylabel(ylabels[i])
+        # axs[row,col].set_xlabel(" ")
 
-        xxlim = axs[row,col].get_xlim()
-        #print(xxlim)
+        # Adding shadings
         fill_color = (1., 0.972549, 0.862745) # named color "cornsilk" in ncl
         axs[row,col].fill_between([ 0.5, 8.5], ranges[i][0], ranges[i][1], facecolor=fill_color, alpha=0.5)
         axs[row,col].fill_between([16.5, 19.5], ranges[i][0], ranges[i][1], facecolor=fill_color, alpha=0.5)
-        #ax.axhspan(0, 1, facecolor="lightgrey", alpha=0.5)
-
-        #ax.axvspan(2000, 2009, 0, 1, facecolor="lightgrey", alpha=0.5)
-
-        # ytickslocs     = [  0,    50,  100,   150,  200,   250,  300,   350,  400,  450]
-        # yticklabels    = ["0.0", "0.5",  "1.0", "1.5",  "2.0", "2.5",  "3.0", "3.5",  "4.0", "4.5"]
-
-
-        # axs[row,col].set(yticks=ytickslocs, yticklabels=yticklabels)
-
-        axs[row,col].set_ylabel(ylabels[i])
-        axs[row,col].set_xlabel(" ")
-        # axs[row,col].legend(numpoints=1, loc="best", frameon=False)
-
-        axs2.set_ylabel(ylabels_R[i])
-
 
         xtickslocs     = [    0,   1,      2,  3,      4,  5,      6,  7,      8,  9, 
                              10,  11,     12,  13,    14, 15,     16, 17,     18     ]
         xticklabels    = ["2001", "", "2003", "", "2005", "", "2007", "", "2009", "",
                           "2011", "", "2013", "", "2015", "", "2017", "", "2019"     ]
         # plt.setp(axs[row,col].get_xticklabels(), visible=False)
-        axs[row,col].get_xaxis().set_visible(False)
+        
         if row == 0 and col ==0:
             axs[row,col].legend(numpoints=1, loc="best", frameon=False) # loc=(0.7, 0.8) 
         else:
@@ -174,11 +146,26 @@ def Fig3_boxplot(start_yr,var_names,ylabels,ylabels_R,ranges,ranges_diff):
 
         if row == 1:    
             # plt.setp(axs[row,col].get_xticklabels(), visible=False)
-            axs[row,col].get_xaxis().set_visible(True)
+            # axs[row,col].get_xaxis().set_visible(True)
             axs[row,col].set(xticks=xtickslocs, xticklabels=xticklabels)
+        else:
+            axs[row,col].get_xaxis().set_visible(False)
 
-        axs[row,col].text(0.05, 0.95, orders[i],transform=axs[row,col].transAxes, fontsize=12, verticalalignment='top', bbox=props) # 
+        axs[row,col].text(0.05, 0.95, orders[i],transform=axs[row,col].transAxes, fontsize=14, verticalalignment='top', bbox=props) # 
 
+
+        # # -------------------- lines ---------------------
+        # # read line values
+        # medians_gw = df_gw.groupby(['year'])['var'].median().values
+        # medians_fd = df_fd.groupby(['year'])['var'].median().values
+
+        # # Plotting boxplot
+        # axs2 = axs[row,col].twinx()
+        # axs2.plot(medians_gw-medians_fd, ls="-", color=almost_black, label="GW-FD")
+        # #align_yaxis(ax, 0, ax2, 0)
+
+        # axs2.set_ylim(ranges_diff[i])
+        # axs2.set_ylabel(ylabels_R[i])
        
         # for ind, label in enumerate(axs[row,col].get_xticklabels()):
         #     if ind % 2 == 0:  # every 2nd label
@@ -191,14 +178,14 @@ def Fig3_boxplot(start_yr,var_names,ylabels,ylabels_R,ranges,ranges_diff):
         #         axs[row,col].set(xticks=xtickslocs, xticklabels=xticklabels)
 
     fig.savefig("./plots/plot_boxplots.png", bbox_inches='tight', dpi=300, pad_inches=2) # 
-
+ 
 
 if __name__ == "__main__":
 
 
     var_names   = ["deltaT","EF","TVeg","Fwsoil"]
-    ylabels     = ["ΔT (oC)","EF (-)","Et (mm d-1)","β (-)"]
-    ylabels_R   = ["ΔT_GW - ΔT_FD (oC)","ΔEF (-)","ΔEt (mm d-1)","Δβ (-)"]
+    ylabels     = ["ΔT ($^{o}$C)","EF (-)","Et (mm d-1)","β (-)"]
+    ylabels_R   = ["ΔT_GW - ΔT_FD ($^{o}$C)","ΔEF (-)","ΔEt (mm d-1)","Δβ (-)"]
     ranges      = [[-0.5, 5], [0., 0.8], [0., 3.8], [0., 1.05]]
     ranges_diff = [[-0.8, 0.], [0., 0.2], [0., 1.1], [0., 0.4]]
     start_yr    = 2000
